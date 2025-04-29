d5a9324a76d327754ca8af2191890d7d1c5d39ac
Bruno Henrique Milhorini <m.brunoh@hotmail.com>
2025-04-29 12:15:02 -0300

    #

--- main.py
+++ main.py
@@ -3,35 +3,33 @@ from time import sleep
 from os import system
 
 ADMIN_USER = "admin"
-ADMIN_CONTRASEÑA = "admin"
+ADMIN_CONTRASENA = "admin"
 
 def login():
    validacion = False
-   intento = 0
+   intentos = 0
    while intentos < 3 and validacion == False: 
       user = input('Ingrese nombre de Usuario: ')
-      contraseña = maskpass.advpass('Ingrese la Contraseña: ')
-      if user != ADMIN_USER or contraseña != ADMIN_CONTRASEÑA:
-         intento += 1
-         print(f'Nombre de Usuario o contraseña incorrecta ({intento}/3).')
+      contrasena = maskpass.advpass('Ingrese la Contraseña: ')
+      if user != ADMIN_USER or contrasena != ADMIN_CONTRASENA:
+         intentos += 1
+         print(f'Nombre de Usuario o contraseña incorrecta. ({intentos}/3)')
       else:
          validacion = True
    if validacion == False:
-      print('Haz alcanzado el limtie de intentos, se cerrara el programa.')
-   else:
-      print('Gracias por usar nuestro sistema.')
+      print('Haz alcanzado el limite de intentos, se cerrara el programa.')
    return validacion
 
-def cartel():
-   print("\n....en construcción...")
+def en_construccion():
+   print("En construcción...")
 
 def mostrar_menu_principal():
    print('---------- MENU PRINCIPAL ----------')
-   print("1- Gestion de Aerolineas.")
-   print("2- Aprobar/Denegar Promiciones")
-   print("3- Gestion de Novedades")
-   print("4- Reportes")
-   print("0- Salir\n")
+   print("1 - Gestión de Aerolíneas.")
+   print("2 - Aprobar/Denegar Promociones")
+   print("3 - Gestión de Novedades")
+   print("4 - Reportes")
+   print("0 - Salir\n")
 
 def Submenu1():
       print("\n........Gestion de Aerolineas.........")
@@ -44,7 +42,7 @@ def Submenu1():
 
 def Submenu2():
       print("\n........Aprobar/Denegar Promociones.........")
-      cartel()
+      en_construccion()
 
 
 def Submenu3():
@@ -73,26 +71,28 @@ def MENU2():
          opc2 = int(input("Ingreso Invalido - reintente ... "))
       
       match opc2:
-         case 1: cartel()
-         case 2: cartel()
-         case 3: cartel()
+         case 1: en_construccion()
+         case 2: en_construccion()
+         case 3: en_construccion()
          case 4: print(" \nVolviendo al menú principal...")
 
 def menuAdmin():
    opc = 1
-   while (opc != 0 ):
+   while (opc != 0):
       mostrar_menu_principal()
-      opc = int(input("Ingrese su opcion: "))
+      opc = int(input("Ingrese su opción: "))
       match opc:
          case 1: Submenu1()  
          case 2: Submenu2()  
          case 3: Submenu3()  
          case 4: Submenu4()  
-         case _: print('Opcion inválida!')
+         case 0: pass
+         case _: print('Opción inválida!')
 
-
-##inicio
+## INÍCIO
 def main():
+   system('cls')
    if login() == True:
       menuAdmin()
+      print('Gracias por usar nuestro sistema.')
 main()
