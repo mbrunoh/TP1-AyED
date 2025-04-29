614fbbe4c487a1be9e92b033e0a09f1c0084c4d4
Bruno Henrique Milhorini <m.brunoh@hotmail.com>
2025-04-29 13:43:55 -0300

    #

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from os import system
    login()
       validacion: bolean
       intentos: int
-      user, contrasena: char
+      user, contrasena: string
    menu_administrador()
       opc: int
    submenu_1()
